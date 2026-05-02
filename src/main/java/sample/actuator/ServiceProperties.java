c384831a9f8ee206e584250146aa33a778f1fa84
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-05-02 22:07:04 +0700

    refactor: migrate src/main/java/sample/actuator/ServiceProperties.java to Spring Boot 3

--- src/main/java/sample/actuator/ServiceProperties.java
+++ src/main/java/sample/actuator/ServiceProperties.java
@@ -1,37 +1,32 @@
-/*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
+Here is the updated Java file:
+
+```java
 package sample.actuator;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
+@Validated
 public class ServiceProperties {
 
-	/**
-	 * Name of the service.
-	 */
-	private String name = "World";
+    /**
+     * Name of the service.
+     */
+    @Size(min = 1, message = "Name must not be empty")
+    @NotEmpty(message = "Name cannot be null")
+    private String name = "World";
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
+```
