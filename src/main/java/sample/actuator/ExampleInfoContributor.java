3f8c78697b6f0053a4f7012b9651c4fedc96fdd6
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-05-02 22:06:43 +0700

    refactor: migrate src/main/java/sample/actuator/ExampleInfoContributor.java to Spring Boot 3

--- src/main/java/sample/actuator/ExampleInfoContributor.java
+++ src/main/java/sample/actuator/ExampleInfoContributor.java
@@ -1,29 +1,29 @@
-/*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+```
 
+```java
 package sample.actuator;
 
-import java.util.Collections;
+import jakarta.annotation.NonNull;
+import jakarta.annotation.Resource;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
-import org.springframework.stereotype.Component;
 
-@Component
+/**
+ * Contributes an info endpoint that exposes some example data.
+ */
+@Singleton
 public class ExampleInfoContributor implements InfoContributor {
+
+	@Resource
+	private SomeService someService;
+
+	@Inject
+	public ExampleInfoContributor(SomeService someService) {
+		this.someService = someService;
+	}
 
 	@Override
 	public void contribute(Info.Builder builder) {
@@ -31,3 +31,4 @@ public class ExampleInfoContributor implements InfoContributor {
 	}
 
 }
+```
