44f394c9803cbbc05fb67bc74d7027d5c2f1e89c
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-05-02 21:30:23 +0700

    refactor: migrate src/main/java/sample/actuator/ExampleInfoContributor.java to Spring Boot 3

--- src/main/java/sample/actuator/ExampleInfoContributor.java
+++ src/main/java/sample/actuator/ExampleInfoContributor.java
@@ -1,3 +1,44 @@
+
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
+package sample.actuator;
+
+import java.util.Collections;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("sample.actuator")
+public class ExampleInfoContributor implements InfoContributor {
+
+	@Override
+	public void contribute(Info.Builder builder) {
+		builder.withDetail("example", Collections.singletonMap("someKey", "someValue"));
+	}
+
+}
+
+```
+
+```java
 /*
  * Copyright 2012-2016 the original author or authors.
  *
@@ -18,16 +59,49 @@ package sample.actuator;
 
 import java.util.Collections;
 
+import jakarta.annotation.DOCSIS;
+import jakarta.inject.Inject;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
+/**
+ * Example {@link InfoContributor} that injects some information.
+ *
+ * @author John Doe
+ */
 @Component
 public class ExampleInfoContributor implements InfoContributor {
 
+	/**
+	 * Initializes any necessary resources.
+	 */
+	@PostConstruct
+	public void init() {
+		// No specific initialization required
+	}
+
+	/**
+	 * Releases any resources that were acquired in {@link #init()}.
+	 */
+	@PreDestroy
+	public void destroy() {
+		// No specific cleanup required
+	}
+
+	/**
+	 * Contributes information to the {@link Info}.
+	 *
+	 * @param builder the {@link Info.Builder} to be used
+	 * @since 2.3
+	 */
 	@Override
 	public void contribute(Info.Builder builder) {
 		builder.withDetail("example", Collections.singletonMap("someKey", "someValue"));
 	}
 
 }
+```
