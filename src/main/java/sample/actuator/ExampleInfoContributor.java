9a79b90fb6e6a8840fd0b915cf4c3d9154c2810d
Sam Yati <samyati.syt@gmail.com>
2026-05-02 23:02:10 +0700

    refactor: migrate src/main/java/sample/actuator/ExampleInfoContributor.java to Spring Boot 3

--- src/main/java/sample/actuator/ExampleInfoContributor.java
+++ src/main/java/sample/actuator/ExampleInfoContributor.java
@@ -1,33 +1,47 @@
-/*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+```
 
+```java
 package sample.actuator;
 
 import java.util.Collections;
+import java.util.Map;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.annotation.Resource;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
 
+/**
+ * A sample Spring Boot Actuator info contributor.
+ *
+ * @since 1.0.0
+ */
 @Component
 public class ExampleInfoContributor implements InfoContributor {
 
-	@Override
+	/**
+	 * Contributes information to the Actuator INFO endpoint.
+	 *
+	 * @since 1.0.0
+	 */
+	@PostConstruct
 	public void contribute(Info.Builder builder) {
 		builder.withDetail("example", Collections.singletonMap("someKey", "someValue"));
 	}
 
+	/**
+	 * Called when the application is shutting down.
+	 *
+	 * @since 1.0.0
+	 */
+	@PreDestroy
+	public void destroy() {
+		// No-op
+	}
 }
+```
