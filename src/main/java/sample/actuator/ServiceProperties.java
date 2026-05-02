dade474bcd10a37c1e0fe42e0443a179b999f202
Sam Yati <samyati.syt@gmail.com>
2026-05-02 23:02:56 +0700

    refactor: migrate src/main/java/sample/actuator/ServiceProperties.java to Spring Boot 3

--- src/main/java/sample/actuator/ServiceProperties.java
+++ src/main/java/sample/actuator/ServiceProperties.java
@@ -1,37 +1,50 @@
+
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * 	http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
  */
-
 package sample.actuator;
 
+import jakarta.annotation.NotNull;
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.Size;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
 public class ServiceProperties {
 
-	/**
-	 * Name of the service.
-	 */
-	private String name = "World";
+    /**
+     * Name of the service.
+     */
+    @Size(min = 1, message = "The service name must not be empty")
+    @NotBlank(message = "The service name must not be empty")
+    private String name = "World";
 
-	public String getName() {
-		return this.name;
-	}
+    /**
+     * Gets the service name.
+     *
+     * @return the service name
+     */
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
+    /**
+     * Sets the service name.
+     *
+     * @param name the service name to set
+     */
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
 }
