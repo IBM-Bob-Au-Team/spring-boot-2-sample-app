c569b85eee3a7c211124399679bcd81dfc1e7b6a
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-05-02 21:51:33 +0700

    refactor: migrate src/main/java/sample/actuator/ServiceProperties.java to Spring Boot 3

--- src/main/java/sample/actuator/ServiceProperties.java
+++ src/main/java/sample/actuator/ServiceProperties.java
@@ -1,37 +1,55 @@
+
 /*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * End of file
  */
 
+```java
 package sample.actuator;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints. SIZE;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * Properties for the service.
+ *
+ * @see ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
+ */
 @ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
+@Validated
 public class ServiceProperties {
 
 	/**
 	 * Name of the service.
+	 *
+	 * @see #setName(String)
+	 * @see #getName()
 	 */
+	@NotNull(message = "The name must not be null")
+	@SIZE(min = 1, message = "The name must not be empty")
 	private String name = "World";
 
-	public String getName() {
-		return this.name;
-	}
-
+	/**
+	 * Sets the name of the service.
+	 *
+	 * @param name The new name of the service.
+	 * @see #getName()
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Returns the name of the service.
+	 *
+	 * @return The name of the service.
+	 * @see #setName(String)
+	 */
+	public String getName() {
+		return this.name;
+	}
+
 }
+```
