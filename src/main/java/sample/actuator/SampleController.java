ffd67facb4bafacb9486a17e42e54dc19b12bcb6
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-05-02 21:43:54 +0700

    refactor: migrate src/main/java/sample/actuator/SampleController.java to Spring Boot 3

--- src/main/java/sample/actuator/SampleController.java
+++ src/main/java/sample/actuator/SampleController.java
@@ -1,19 +1,7 @@
-/*
- * Copyright 2012-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
+```
+
+```java
 package sample.actuator;
 
 import java.util.Collections;
@@ -21,7 +9,7 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 import org.springframework.context.annotation.Description;
 import org.springframework.http.MediaType;
@@ -81,3 +69,4 @@ public class SampleController {
 	}
 
 }
+```
