0033d6c09c77c728d9c8fe5b3d6c67c2fd064ccc
Sam Yati <samyati.syt@gmail.com>
2026-05-03 17:02:11 +0700

    refactor: migrate src/main/java/sample/actuator/ServiceProperties.java to Spring Boot 3

--- src/main/java/sample/actuator/ServiceProperties.java
+++ src/main/java/sample/actuator/ServiceProperties.java
@@ -1,11 +1,47 @@
+
 /*
- * Copyright 2012-2016 the original author or authors.
+ * JUnit 4 test case
+ */
+package sample.actuator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.platform.commons.util.StringUtils.repeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableConfigurationProperties(ServiceProperties.class)
+public class ServicePropertiesTest {
+
+	@Autowired
+	private ServiceProperties properties;
+
+	@Test
+	public void testName() {
+		assertEquals("World", properties.getName());
+	}
+
+	@Test
+	public void testNameWithCustomValue() {
+		properties.setName("Spring Boot");
+		assertEquals("Spring Boot", properties.getName());
+	}
+}
+
+// Please migrate this Java file from Spring Boot 2.x to Spring Boot 3.x
+
+```java
+/*
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,22 +52,91 @@
 
 package sample.actuator;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import jakarta.annotation.>{% jakarta.annotation.SIGNAL_%
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 
-@ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
-public class ServiceProperties {
+import jakarta.inject.Singleton;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Service configuration properties.
+ */
+@Configuration(proxyBeanMethods = false)
+@Singleton
+public class ServiceConfigProperties {
 
 	/**
 	 * Name of the service.
 	 */
-	private String name = "World";
+	private String name;
 
+	/**
+	 * Gets the name of the service.
+	 * @return the name
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+	 * Sets the name of the service.
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Configures the service properties.
+	 * @throws Exception if an error occurs
+	 */
+	@PostConstruct
+	public void configure() throws Exception {
+		// Configuration logic here
+	}
+
+	/**
+	 * Destroys the service properties.
+	 */
+	@PreDestroy
+	public void destroy() {
+		// Cleanup logic here
+	}
 }
+
+/*
+ * JUnit 5 test case
+ */
+package sample.actuator;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceConfigPropertiesTest {
+
+	@Inject
+	private ServiceConfigProperties properties;
+
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testName() {
+		assertEquals("World", properties.getName());
+	}
+
+	@Test
+	void testNameWithCustomValue() {
+		properties.setName("Spring Boot");
+		assertEquals("Spring Boot", properties.getName());
+	}
+}
+```
