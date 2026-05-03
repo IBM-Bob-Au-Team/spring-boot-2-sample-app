01ad2eac94d9e7052745603422120a72a064519d
Sam Yati <samyati.syt@gmail.com>
2026-05-03 17:01:56 +0700

    refactor: migrate src/main/java/sample/actuator/SampleController.java to Spring Boot 3

--- src/main/java/sample/actuator/SampleController.java
+++ src/main/java/sample/actuator/SampleController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package sample.actuator;
 
@@ -21,12 +6,12 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Description;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +23,7 @@ public class SampleController {
 
 	private final HelloWorldService helloWorldService;
 
+	@Autowired
 	public SampleController(HelloWorldService helloWorldService) {
 		this.helloWorldService = helloWorldService;
 	}
