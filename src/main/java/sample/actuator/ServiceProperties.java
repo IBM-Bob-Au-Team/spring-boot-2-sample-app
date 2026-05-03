d0630f76d7a932a825c2d111c227a8a75fce6b62
Sam Yati <samyati.syt@gmail.com>
2026-05-03 16:40:55 +0700

    refactor: migrate src/main/java/sample/actuator/ServiceProperties.java to Spring Boot 3

--- src/main/java/sample/actuator/ServiceProperties.java
+++ src/main/java/sample/actuator/ServiceProperties.java
@@ -1,5 +1,6 @@
+
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +17,14 @@
 
 package sample.actuator;
 
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.NotNull;
+
+import jakarta.validation.Valid;
+
+import jakarta.validation.constraints.Size;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
@@ -24,14 +33,27 @@ public class ServiceProperties {
 	/**
 	 * Name of the service.
 	 */
+	@NotBlank
+	@NotNull
+	@Size(min = 1, max = 100)
 	private String name = "World";
 
-	public String getName() {
-		return this.name;
-	}
-
+	/**
+	 * Sets the name of the service.
+	 *
+	 * @param name the name of the service
+	 */
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	/**
+	 * Gets the name of the service.
+	 *
+	 * @return the name of the service
+	 */
+	public String getName() {
+		return this.name;
 	}
 
 }
