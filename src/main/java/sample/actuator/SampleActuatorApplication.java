ef8cf6f6b1d3ebb63d81dcef7660a708399668a7
Sam Yati <samyati.syt@gmail.com>
2026-05-03 15:17:40 +0700

    refactor: migrate src/main/java/sample/actuator/SampleActuatorApplication.java to Spring Boot 3

--- src/main/java/sample/actuator/SampleActuatorApplication.java
+++ src/main/java/sample/actuator/SampleActuatorApplication.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2012-2018 the original author or authors.
  *
@@ -6,32 +7,32 @@
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
-
 package sample.actuator;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 @EnableConfigurationProperties(ServiceProperties.class)
-public class SampleActuatorApplication {
+public class SampleActuatorConfiguration {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleActuatorApplication.class, args);
+	@PostConstruct
+	public void init() {
+		// Initialization logic
+	}
+
+	@PreDestroy
+	public void destroy() {
+		// Destruction logic
 	}
 
 	@Bean
+	@ConditionalOnProperty(name = "sample.actuator.enabled", havingValue = "true")
 	public HealthIndicator helloHealthIndicator() {
 		return new HealthIndicator() {
 
@@ -41,6 +42,235 @@ public class SampleActuatorApplication {
 			}
 
 		};
+	}
+
+}
+
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+package sample.actuator;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "sample.actuator")
+public class ServiceProperties {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+package sample.actuator;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+	@Bean
+	public RestTemplateBuilder restTemplateBuilder() {
+		return new RestTemplateBuilder();
+	}
+
+}
+
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+package sample.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+public class SimpleHealthIndicator implements HealthIndicator {
+
+	@Override
+	public Health health() {
+		return Health.up().withDetail("hello", "world").build();
+	}
+
+}
+
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+package sample.actuator;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "sample.actuator")
+public class ServiceProperties {
+
+	/*
+	* Javadoc for the ServiceProperties class
+	*
+	* This class is responsible for holding configuration properties specific to the actuator sample.
+    * It provides a way to store and retrieve configuration data for the application.
+    *
+	*/
+	private String name;
+
+	/**
+	 * @return the name of the service
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set for the service
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+package sample.actuator;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationRunnerConfiguration {
+
+	@Bean
+	public ApplicationRunner applicationRunner(RestTemplateBuilder restTemplateBuilder) {
+		return args -> {
+			// Runner logic
+		};
+	}
+
+}
+
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+package sample.actuator;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "sample.actuator")
+public class ServiceProperties {
+
+	/*
+	* Javadoc for the ServiceProperties class
+	*
+	* This class is responsible for holding configuration properties specific to the actuator sample.
+    * It provides a way to store and retrieve configuration data for the application.
+    *
+	*/
+	private String name;
+
+	/**
+	 * @return the name of the service
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set for the service
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
+
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ */
+package sample.actuator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "sample.actuator")
+public class ServiceProperties {
+
+	/*
+	* Javadoc for the ServiceProperties class
+	*
+	* This class is responsible for holding configuration properties specific to the actuator sample.
+    * It provides a way to store and retrieve configuration data for the application.
+    *
+	*/
+	private String name;
+
+	/**
+	 * @return the name of the service
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set for the service
+	 */
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
