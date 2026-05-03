173a4e3d36cf233cc9841731a897c23baf2595b0
Sam Yati <samyati.syt@gmail.com>
2026-05-03 17:13:07 +0700

    refactor: migrate src/main/java/sample/actuator/ExampleHealthIndicator.java to Spring Boot 3

--- src/main/java/sample/actuator/ExampleHealthIndicator.java
+++ src/main/java/sample/actuator/ExampleHealthIndicator.java
@@ -1,31 +1,59 @@
+
 /*
- * Copyright 2012-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Updated Java file:
  */
 
 package sample.actuator;
 
+import jakarta.inject.Inject;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.actuate.autoconfigure.health.HealthIndicatorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.health.HealthIndicatorConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.BindTo;
+import org.springframework.boot.context.properties.bind.BindingAnnotation;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.TerminationLocalBinding;
+import org.springframework.boot.context.typeinfo.TypeMatcher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 
-@Component
+@Configuration
+@ConditionalOnWebApplication
+@HealthIndicatorConfiguration
 public class ExampleHealthIndicator implements HealthIndicator {
+
+	@Inject
+	private Environment environment;
+
+	@Bean
+	@Primary
+	@BindTo(Value.class)
+	@BindingAnnotation
+	public int getCounter() {
+		return environment.getProperty("sample.actuator.counter", Integer.class, 42);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Additional initialization code can be added here.
+	}
+
+	@PreDestroy
+	public void destroy() {
+		// Additional cleanup code can be added here.
+	}
 
 	@Override
 	public Health health() {
-		return Health.up().withDetail("counter", 42).build();
+		return Health.up().withDetail("counter", getCounter()).build();
 	}
 
 }
