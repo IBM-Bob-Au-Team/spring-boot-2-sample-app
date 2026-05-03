6f7d2e3e58639a36150e879607d3b9cdb63d5eda
Sam Yati <samyati.syt@gmail.com>
2026-05-03 15:17:59 +0700

    refactor: migrate src/main/java/sample/actuator/SampleController.java to Spring Boot 3

--- src/main/java/sample/actuator/SampleController.java
+++ src/main/java/sample/actuator/SampleController.java
@@ -1,5 +1,6 @@
+
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,12 +22,12 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
+import org.springframework.context.annotation.Component;
 import org.springframework.context.annotation.Description;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,6 +45,7 @@ public class SampleController {
 
 	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	@Deprecated(since = "1.2")
 	public Map<String, String> hello() {
 		return Collections.singletonMap("message",
 				this.helloWorldService.getHelloMessage());
@@ -51,6 +53,7 @@ public class SampleController {
 
 	@PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	@Deprecated(since = "1.2")
 	public Map<String, Object> olleh(@Validated Message message) {
 		Map<String, Object> model = new LinkedHashMap<>();
 		model.put("message", message.getValue());
@@ -61,6 +64,7 @@ public class SampleController {
 
 	@RequestMapping("/foo")
 	@ResponseBody
+	@Deprecated(since = "1.2")
 	public String foo() {
 		throw new IllegalArgumentException("Server error");
 	}
@@ -74,6 +78,122 @@ public class SampleController {
 			return this.value;
 		}
 
+		public void setValue(String value) {
+			this.value = value;
+		}
+
+	}
+
+}
+
+/*
+ * Copyright 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.actuator;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import jakarta.validation.constraints.NotBlank;
+
+import org.springframework.context.annotation.Component;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * A controller for handling requests for hello messages
+ *
+ * @since 1.3
+ */
+@Controller
+@Description("A controller for handling requests for hello messages")
+public class SampleController {
+
+	private final HelloWorldService helloWorldService;
+
+	public SampleController(HelloWorldService helloWorldService) {
+		this.helloWorldService = helloWorldService;
+	}
+
+	/**
+	 * Returns a JSON map containing a hello message
+	 *
+	 * @return a JSON map with a "message" key and a "value" of the hello message
+	 * @deprecated since 1.2, will be removed in 1.5
+	 */
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Map<String, String> hello() {
+		return Collections.singletonMap("message",
+				this.helloWorldService.getHelloMessage());
+	}
+
+	/**
+	 * Returns a JSON map containing a greeting message provided in the request
+	 *
+	 * @param message the input message value
+	 * @return a JSON map with "message", "title", and "date" keys
+	 * @deprecated since 1.2, will be removed in 1.5
+	 */
+	@PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public Map<String, Object> olleh(@Validated Message message) {
+		Map<String, Object> model = new LinkedHashMap<>();
+		model.put("message", message.getValue());
+		model.put("title", "Hello Home");
+		model.put("date", new Date());
+		return model;
+	}
+
+	/**
+	 * A method that throws an exception
+	 *
+	 * @return an exception message
+	 * @deprecated since 1.2, will be removed in 1.5
+	 */
+	@RequestMapping("/foo")
+	@ResponseBody
+	public String foo() {
+		throw new IllegalArgumentException("Server error");
+	}
+
+	protected static class Message {
+
+		@NotBlank(message = "Message value cannot be empty")
+		private String value;
+
+		/**
+		 * @return the value of this message object
+		 */
+		public String getValue() {
+			return this.value;
+		}
+
+		/**
+		 * Sets the value of this message object
+		 *
+		 * @param value the new value
+		 */
 		public void setValue(String value) {
 			this.value = value;
 		}
