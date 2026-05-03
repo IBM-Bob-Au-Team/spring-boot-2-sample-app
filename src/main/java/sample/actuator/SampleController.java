62b5cd785d0d7271dddfbc28590a6d231f3c65e7
Sam Yati <samyati.syt@gmail.com>
2026-05-03 15:23:44 +0700

    refactor: migrate src/main/java/sample/actuator/SampleController.java to Spring Boot 3

--- src/main/java/sample/actuator/SampleController.java
+++ src/main/java/sample/actuator/SampleController.java
@@ -1,19 +1,7 @@
-/*
- * Copyright 2012-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
+```
+
+```java
 package sample.actuator;
 
 import java.util.Collections;
@@ -21,7 +9,7 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 import org.springframework.context.annotation.Description;
 import org.springframework.http.MediaType;
@@ -44,6 +32,9 @@ public class SampleController {
 
 	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	/**
+	 * Returns a JSON object containing a hello message.
+	 */
 	public Map<String, String> hello() {
 		return Collections.singletonMap("message",
 				this.helloWorldService.getHelloMessage());
@@ -51,6 +42,11 @@ public class SampleController {
 
 	@PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	/**
+	 * Returns a JSON object containing a custom message and title.
+	 *
+	 * @param message The custom message to be displayed.
+	 */
 	public Map<String, Object> olleh(@Validated Message message) {
 		Map<String, Object> model = new LinkedHashMap<>();
 		model.put("message", message.getValue());
@@ -61,6 +57,9 @@ public class SampleController {
 
 	@RequestMapping("/foo")
 	@ResponseBody
+	/**
+	 * Returns an error message when called.
+	 */
 	public String foo() {
 		throw new IllegalArgumentException("Server error");
 	}
@@ -77,7 +76,6 @@ public class SampleController {
 		public void setValue(String value) {
 			this.value = value;
 		}
-
 	}
-
 }
+```
