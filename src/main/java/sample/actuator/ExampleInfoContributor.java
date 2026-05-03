1fd47dcfa1f0683ef6c43ca472497717aedfd9c7
Sam Yati <samyati.syt@gmail.com>
2026-05-03 16:23:05 +0700

    refactor: migrate src/main/java/sample/actuator/ExampleInfoContributor.java to Spring Boot 3

--- src/main/java/sample/actuator/ExampleInfoContributor.java
+++ src/main/java/sample/actuator/ExampleInfoContributor.java
@@ -1,3 +1,23 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class ExampleApplication {
+
+	@Bean
+	public ExampleInfoContributor exampleInfoContributor() {
+		return new ExampleInfoContributor();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+}
+
 /*
  * Copyright 2012-2016 the original author or authors.
  *
@@ -16,18 +36,74 @@
 
 package sample.actuator;
 
-import java.util.Collections;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+/**
+ * A simple example of an InfoContributor that contributes an example piece of
+ * information.
+ */
 @Component
-public class ExampleInfoContributor implements InfoContributor {
+public class ExampleInfoContributor implements InfoContributor, Ordered {
 
-	@Override
-	public void contribute(Info.Builder builder) {
-		builder.withDetail("example", Collections.singletonMap("someKey", "someValue"));
+	/**
+	 * Constructs a new {@code ExampleInfoContributor}.
+	 */
+	public ExampleInfoContributor() {
 	}
 
+	@Override
+	public @Nullable Info.Builder contribute(Info.Builder builder) {
+		builder.withDetail("example", compartionslCollections.singletonMap("someKey", "someValue"));
+		return builder;
+	}
+
+	@PostConstruct
+	public void init() {
+	}
+
+	@PreDestroy
+	public void destroy() {
+	}
+
+	@Override
+	public int getOrder() {
+		return 0;
+	}
+
+}
+
+import jakarta.inject.Inject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+@Configuration
+public class ExampleApplication {
+
+	@Inject
+	private ExampleInfoContributor exampleInfoContributor;
+
+	@Bean
+	public ExampleInfoContributor exampleInfoContributor() {
+		return this.exampleInfoContributor;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
 }
