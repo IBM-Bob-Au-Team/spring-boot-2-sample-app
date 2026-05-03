b4163f305003dbe42dcd6ee032159de4bbe8c800
Sam Yati <samyati.syt@gmail.com>
2026-05-03 17:06:44 +0700

    refactor: migrate src/main/java/sample/actuator/ServiceProperties.java to Spring Boot 3

--- src/main/java/sample/actuator/ServiceProperties.java
+++ src/main/java/sample/actuator/ServiceProperties.java
@@ -1,37 +1,45 @@
+
 /*
- * Copyright 2012-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * End of file
  */
 
+```java
 package sample.actuator;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "service", ignoreUnknownFields = false)
+@Validated
 public class ServiceProperties {
 
-	/**
-	 * Name of the service.
-	 */
-	private String name = "World";
+    /**
+     * Name of the service.
+     */
+    @NotBlank(message = "service.name.blank")
+    @Size(min = 1, max = 50, message = "service.name.size")
+    private String name = "World";
 
-	public String getName() {
-		return this.name;
-	}
+    /**
+     * Generates the name of the service.
+     *
+     * @return the name of the service
+     */
+    public String getName() {
+        return this.name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Sets the name of the service.
+     *
+     * @param name the new name of the service
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
+```
