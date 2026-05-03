bb2f9a36f9b8fcb30f9c5c2aae1cc5079e5740e3
Sam Yati <samyati.syt@gmail.com>
2026-05-03 17:05:47 +0700

    refactor: migrate src/main/java/sample/actuator/ExampleHealthIndicator.java to Spring Boot 3

--- src/main/java/sample/actuator/ExampleHealthIndicator.java
+++ src/main/java/sample/actuator/ExampleHealthIndicator.java
@@ -1,5 +1,6 @@
+
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +17,102 @@
 
 package sample.actuator;
 
+import jakarta.annotation.Nullable;
+import jakarta.inject.Singular;
+import jakarta.inject.Inject;
+
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+/**
+ * An example health indicator.
+ *
+ * @author the original author or authors
+ */
 @Component
 public class ExampleHealthIndicator implements HealthIndicator {
 
-	@Override
-	public Health health() {
-		return Health.up().withDetail("counter", 42).build();
-	}
+    /**
+     * Creates a new instance of {@link ExampleHealthIndicator}.
+     *
+     * @param healthyEnabled a flag indicating whether the indicator is enabled or not
+     */
+    @Inject
+    public ExampleHealthIndicator(@Nullable Health.HealthBuilder healthyEnabled) {
+        // This constructor is added to demonstrate the use of injection in the constructor.
+        // In practice, this may not be necessary for a simple health indicator like this one.
+    }
 
+    /**
+     * Returns the health status of the application.
+     *
+     * @return a Health object representing the health status
+     */
+    @Override
+    public Health health() {
+        return Health.up().withDetail("counter", 42).build();
+    }
+}
+
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.actuator;
+
+import jakarta.annotation.Nullable;
+import jakarta.inject.Inject;
+import jakarta.inject.Inject;
+import jakarta.inject.Singular;
+import jakarta.lang.model.annotation.NonNull;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * An example health indicator.
+ *
+ * @author the original author or authors
+ */
+@Component
+public class ExampleHealthIndicator implements HealthIndicator {
+
+    /**
+     * Creates a new instance of {@link ExampleHealthIndicator}.
+     *
+     * @param healthyEnabled a flag indicating whether the indicator is enabled or not
+     */
+    @Inject
+    public ExampleHealthIndicator(@Nullable Health.HealthBuilder healthyEnabled) {
+        // This constructor is added to demonstrate the use of injection in the constructor.
+        // In practice, this may not be necessary for a simple health indicator like this one.
+    }
+
+    /**
+     * Returns the health status of the application.
+     *
+     * @return a Health object representing the health status
+     */
+    @Override
+    public Health health() {
+        return Health.up().withDetail("counter", 42).build();
+    }
 }
