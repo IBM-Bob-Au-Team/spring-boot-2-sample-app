aa7ec8d2a8bb1b8a4d3aa8b07586b8827be5e16c
Sam Yati <samyati.syt@gmail.com>
2026-05-03 15:07:16 +0700

    refactor: migrate src/main/java/sample/actuator/SampleActuatorApplication.java to Spring Boot 3

--- src/main/java/sample/actuator/SampleActuatorApplication.java
+++ src/main/java/sample/actuator/SampleActuatorApplication.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2012-2018 the original author or authors.
  *
@@ -8,30 +9,51 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+```
 
+```java
 package sample.actuator;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import jakarta.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceHealthIndicator;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ServiceProperties.class)
 public class SampleActuatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleActuatorApplication.class, args);
+	@Resource
+	private DataSource dataSource;
+
+	@PostConstruct
+	public void init() {
+		// Initialize the application
 	}
 
 	@Bean
+	@ConditionalOnMissingBean
+	public DataSourceHealthIndicator dataSourceHealthIndicator() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:test");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+
+		return new DataSourceHealthIndicator(dataSource);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean
 	public HealthIndicator helloHealthIndicator() {
 		return new HealthIndicator() {
 
@@ -44,3 +66,4 @@ public class SampleActuatorApplication {
 	}
 
 }
+```
