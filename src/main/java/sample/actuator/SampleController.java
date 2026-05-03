b98fd291991f71646c08336867ee1d7753d828ff
Sam Yati <samyati.syt@gmail.com>
2026-05-03 16:23:27 +0700

    refactor: migrate src/main/java/sample/actuator/SampleController.java to Spring Boot 3

--- src/main/java/sample/actuator/SampleController.java
+++ src/main/java/sample/actuator/SampleController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012-2017 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package sample.actuator;
 
@@ -21,19 +6,16 @@ import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import javax.validation.constraints.NotBlank;
-
-import org.springframework.context.annotation.Description;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@Description("A controller for handling requests for hello messages")
+@org.springframework.context.annotation.Description("A controller for handling requests for hello messages")
 public class SampleController {
 
 	private final HelloWorldService helloWorldService;
@@ -44,6 +26,9 @@ public class SampleController {
 
 	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	/**
+	 * Returns a JSON object containing a hello message.
+	 */
 	public Map<String, String> hello() {
 		return Collections.singletonMap("message",
 				this.helloWorldService.getHelloMessage());
@@ -51,6 +36,9 @@ public class SampleController {
 
 	@PostMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
+	/**
+	 * Returns a JSON object containing a greeting based on the provided message.
+	 */
 	public Map<String, Object> olleh(@Validated Message message) {
 		Map<String, Object> model = new LinkedHashMap<>();
 		model.put("message", message.getValue());
@@ -61,6 +49,9 @@ public class SampleController {
 
 	@RequestMapping("/foo")
 	@ResponseBody
+	/**
+	 * Returns an error message due to a server error.
+	 */
 	public String foo() {
 		throw new IllegalArgumentException("Server error");
 	}
@@ -70,10 +61,20 @@ public class SampleController {
 		@NotBlank(message = "Message value cannot be empty")
 		private String value;
 
+		/**
+		 * Gets the value of the message.
+		 *
+		 * @return the value of the message
+		 */
 		public String getValue() {
 			return this.value;
 		}
 
+		/**
+		 * Sets the value of the message.
+		 *
+		 * @param value the new value of the message
+		 */
 		public void setValue(String value) {
 			this.value = value;
 		}
