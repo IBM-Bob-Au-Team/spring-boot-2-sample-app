d5f78dfbb46265e6e94e30fd18e47383275cee5f
Sam Yati <samyati.syt@gmail.com>
2026-05-03 17:01:28 +0700

    refactor: migrate src/main/java/sample/actuator/HelloWorldService.java to Spring Boot 3

--- src/main/java/sample/actuator/HelloWorldService.java
+++ src/main/java/sample/actuator/HelloWorldService.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2012-2016 the original author or authors.
  *
@@ -16,15 +17,99 @@
 
 package sample.actuator;
 
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Service;
 
 @Service
-public class HelloWorldService {
+public class HelloWorldHealthIndicator implements HealthIndicator {
 
-	
+	@Override
+	public Health.Builder health() {
+		return Health.up().build();
+	}
 
-	public String getHelloMessage() {
-		return "Spring boot says hello from a Docker container";
+}
+
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.actuator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfigurationService {
+
+	private final String greeting;
+
+	public ConfigurationService(@Value("${greeting.default:Hello}") String greeting) {
+		this.greeting = greeting;
+	}
+
+	public String getGreeting() {
+		return this.greeting;
+	}
+
+}
+
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+(* @File copyright.txt)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldHealthCheck implements HealthIndicator {
+
+	private final HelloWorldService helloWorldService;
+
+	public HelloWorldHealthCheck(HelloWorldService helloWorldService) {
+		this.helloWorldService = helloWorldService;
+	}
+
+	@Override
+	public Health.Builder health() {
+		return this.helloWorldService.getHelloMessage().equals("Spring boot says hello from a Docker container") ? Health.up().build() : Health.down(Status.OUT_OF_SERVICE);
 	}
 
 }
